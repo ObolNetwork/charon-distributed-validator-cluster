421f007380aa0fd83aa4fe66e031925c0a16bd8f
corverroos <corver.roos@gmail.com>
2022-03-05 12:19:53 +0200

    support locally built charon binaries

---
+++ build_local.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Build a local charon binary to run in the docker container (instead of official release).
+# This is useful for local development and debugging.
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+if [ -n "${CHARON_REPO}"]; then
+  # Either use the provided a path to the charon repo
+  cd "${CHARON_REPO}"
+else
+  # Or assume it is next to this repo.
+  cd "${SCRIPT_DIR}/../charon"
+fi
+
+GOOS=linux GOARCH=amd64 go build -o "${SCRIPT_DIR}/charon"
