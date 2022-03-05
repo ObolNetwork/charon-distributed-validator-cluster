51ddc30fccba4dec07ade2462de5c4c97c88fddc
corverroos <corver.roos@gmail.com>
2022-03-05 14:07:36 +0200

    clean up

--- build_local.sh
+++ build_local.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+set -e
+
 # Build a local charon binary to run in the docker container (instead of official release).
 # This is useful for local testing and debugging.
 
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
-if [ -n "${CHARON_REPO}"]; then
+if [ "${CHARON_REPO}" != "" ]; then
   # Either use the provided a path to the charon repo
   cd "${CHARON_REPO}"
 else
@@ -13,5 +15,12 @@ else
   cd "${SCRIPT_DIR}/../charon"
 fi
 
+MSG="Source: $(git log --pretty=oneline -1)"
+if [[ $(git diff --stat) != '' ]]; then
+  MSG="${MSG} (dirty)"
+fi
+echo "${MSG}"
+echo "Target: ${SCRIPT_DIR}/charon"
+
 GOOS=linux GOARCH=amd64 go build -o "${SCRIPT_DIR}/charon"
-cd -
+cd - &> /dev/null
