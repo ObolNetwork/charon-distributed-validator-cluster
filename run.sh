45d7883311e6b5d1b2ad02e4e8932a4fb149512c
corverroos <corver@obol.tech>
2022-03-29 10:36:10 +0200

    add grafana dashbaord (#16)
    Adds a grafana dashboard. Also fixes race between node0 clean and other node config.
    
    Fixes: #13

--- run.sh
+++ run.sh
@@ -22,29 +22,31 @@ if [ "${GENERATE}" = "true" ] && [ ! -f "/charon/manifest.json" ]; then
   rm /charon/node*/run.sh
 fi
 
-# Get container IP (https://stackoverflow.com/questions/13322485/how-to-get-the-primary-ip-address-of-the-local-machine-on-linux-and-os-x)
-BIND=$(ip route get 1 | awk '{print $NF;exit}')
-
-# Write a config file: charon.yml
-cat <<EOF > charon.yml
-
-data-dir: ${PWD}
-manifest-file: /charon/manifest.json
-monitoring-address: ${BIND}:16001
-validator-api-address: ${BIND}:16002
-p2p-tcp-address: ${BIND}:16003
-p2p-udp-address: ${BIND}:16004
-simnet-validator-mock: ${SIMNET_VMOCK:-true}
-EOF
-
 # If BOOTNODE env var present, resolve ENR via curl/wget
 if [ -n "${BOOTNODE}" ]; then
   while ! ENR=$(wget -qO- "http://${BOOTNODE}:16001/enr" 2>/dev/null); do
     echo "Waiting for http://${BOOTNODE}:16001/enr to become available..."
     sleep 5
   done
-  echo "p2p-bootnodes: '${ENR}'" >> charon.yml
 fi
+
+
+# Get container IP (https://stackoverflow.com/questions/13322485/how-to-get-the-primary-ip-address-of-the-local-machine-on-linux-and-os-x)
+BIND=$(ip route get 1 | awk '{print $NF;exit}')
+
+# Write a config file: charon.yml
+cat <<EOF > charon.yml
+data-dir: ${PWD}
+manifest-file: /charon/manifest.json
+monitoring-address: ${BIND}:16001
+validator-api-address: ${BIND}:16002
+p2p-bootnodes: ${ENR}
+p2p-tcp-address: ${BIND}:16003
+p2p-udp-address: ${BIND}:16004
+simnet: true
+simnet-validator-mock: ${SIMNET_VMOCK:-true}
+EOF
+
 
 # Run charon
 exec charon run
