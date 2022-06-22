d779e27706ce0bff7e6d9f7f5c9782b210ce052e
Dhruv Bodani <dhruvbodani2510@gmail.com>
2022-06-22 19:19:31 +0530

    cleanup

--- lighthouse/run.sh
+++ lighthouse/run.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-while ! curl "http://${NODE}:3610/up" 2>/dev/null; do
-  echo "Waiting for http://${NODE}:3610/up to become available..."
-  sleep 5
-done
-
 echo "Creating testnet config"
 rm -rf /tmp/testnet || true
 mkdir /tmp/testnet/
-curl "http://${NODE}:3610/eth/v1/config/spec" | jq -r .data | yq -P > /tmp/testnet/config.yaml
+curl "http://${NODE}:3600/eth/v1/config/spec" | jq -r .data | yq -P > /tmp/testnet/config.yaml
 echo "0" > /tmp/testnet/deploy_block.txt
 
 for f in /opt/charon/keys/keystore-*.json; do
@@ -22,7 +17,7 @@ done
 echo "Starting lighthouse validator client for ${NODE}"
 exec lighthouse validator \
   --testnet-dir "/tmp/testnet" \
-  --beacon-node "http://${NODE}:3610" \
+  --beacon-node "http://${NODE}:3600" \
   --suggested-fee-recipient "0x0000000000000000000000000000000000000000" \
   --metrics \
   --metrics-address "0.0.0.0" \
