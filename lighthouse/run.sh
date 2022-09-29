519b4a8190e829df4130324cce439866c480853d
xenowits <kumar.abhishek111b@gmail.com>
2022-09-29 14:23:11 +0530

    cleanup

--- lighthouse/run.sh
+++ lighthouse/run.sh
@@ -5,28 +5,25 @@ while ! curl "http://${NODE}:3600/eth/v1/node/health" 2>/dev/null; do
   sleep 5
 done
 
-echo "Creating testnet config"
-rm -rf /tmp/testnet || true
-mkdir /tmp/testnet/
-curl "http://${NODE}:3600/eth/v1/config/spec" | jq -r .data | yq -P > /tmp/testnet/config.yaml
-echo "0" > /tmp/testnet/deploy_block.txt
+# Refer: https://lighthouse-book.sigmaprime.io/advanced-datadir.html
+# Running a lighthouse VC involves two steps which needs to run in order:
+# 1. Loading the validator keys
+# 2. Actually running the VC
 
 for f in /opt/charon/keys/keystore-*.json; do
   echo "Importing key ${f}"
-  cat "$(echo "${f}" | sed 's/json/txt/')" | lighthouse account validator import \
-    --testnet-dir "/tmp/testnet" \
-    --stdin-inputs \
-    --keystore "${f}"
+  lighthouse --network "${ETH2_NETWORK}" account validator import \
+    --reuse-password \
+    --keystore "${f}" \
+    --password-file "${f//json/txt}"
 done
 
 echo "Starting lighthouse validator client for ${NODE}"
-exec lighthouse validator \
-  --testnet-dir "/tmp/testnet" \
-  --beacon-node "http://${NODE}:3600" \
+exec lighthouse --network "${ETH2_NETWORK}" validator \
+  --beacon-nodes "http://${NODE}:3600" \
   --suggested-fee-recipient "0x0000000000000000000000000000000000000000" \
   --metrics \
   --metrics-address "0.0.0.0" \
   --metrics-allow-origin "*" \
   --metrics-port "5064" \
   --use-long-timeouts \
-  --graffiti "Distributed Validator - LH"
